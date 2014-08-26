f7ad046e755ad8e051414d0351daabdadb301703
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-26 15:39:48 -0400

    Making Pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,3 +1,24 @@
 class PicturesController < ApplicationController
+  def index
+    @pictures = [
+      {
+        :title  => "The old church on the coast of White sea",
+        :artist => "Sergey Ershov",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
+      },
+      {
+        :title  => "Sea Power", 
+        :artist => "Stephen Scullion",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
+      }
+
+    ]
+    
+  end
 
 end

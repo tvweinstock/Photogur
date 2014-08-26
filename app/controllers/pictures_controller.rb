defcf9d53f9f7bee01a18decb8d48f2e0deed16e
Tobi Weinstock <tv.weinstock@gmail.com>
2014-08-26 19:44:50 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,50 +1,13 @@
 class PicturesController < ApplicationController
   def index
-
-    @pictures = [
-      {
-      :title  => "The old church on the coast of White sea",
-      :artist => "Sergey Ershov",
-      :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-    },
-    {
-      :title  => "Sea Power", 
-      :artist => "Stephen Scullion",
-      :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-    },
-    {
-      :title  => "Into the Poppies",
-      :artist => "John Wilhelm",
-      :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-    }
-  ]
+    @pictures = Picture.all
   end
 
   def show
-      @pictures = [
-        {
-        :title  => "The old church on the coast of White sea",
-        :artist => "Sergey Ershov",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-      },
-      {
-        :title  => "Sea Power", 
-        :artist => "Stephen Scullion",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-      },
-      {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-      }
-    ]
-
-  @picture = @pictures[params[:id].to_i]
-    
+  @picture = Picture.find(params[:id])
   end
 
   def new
-    
   end
 
   def create
